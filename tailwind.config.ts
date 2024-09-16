05b6b657db69662d99c9010c44e33eeef2c2916d
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-09-16 14:35:22 +0900

    chore: setting custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#1B4192",
+        accent: "#FFA100",
       },
     },
   },
